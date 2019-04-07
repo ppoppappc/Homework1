b72edc8efcfd25325749a45d2e105c739bdc42c8
ppoppappc <ppoppappc@gmail.com>
2019-04-07 20:04:22 +0800

    Initial commit

---
+++ app/src/main/java/ppo/tku/homework1/MainActivity.java
@@ -0,0 +1,25 @@
+package ppo.tku.homework1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView01;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView01 = (TextView) findViewById(R.id.myTextView01);
+        String str_2 = "test 測試 Android TextView";
+        mTextView01.setText(str_2);
+        mTextView01.setTextColor(getResources().getColor(R.color.colorAccent));
+    }
+
+    public void changeColor(View v){
+        mTextView01.setBackgroundColor(getResources().getColor(R.color.blue));
+    }
+}
